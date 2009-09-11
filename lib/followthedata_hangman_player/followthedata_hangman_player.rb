6c10b17dcdc30f25304acce26a51356ae8e7334e
sunkencity <tux@well.com>
2009-09-11 17:47:14 +0200

    some more work

--- lib/followthedata_hangman_player/followthedata_hangman_player.rb
+++ lib/followthedata_hangman_player/followthedata_hangman_player.rb
@@ -1,51 +1,54 @@
 module FollowthedataHangmanPlayer
   class FollowthedataHangmanPlayer
 
+    def initialize
+      @vocals = { "a" => 1, "e" => 0 }
+      @vocals.default = 10
+    end
+    
     def new_game(guesses_left)
-      @curr_dictionary = Array.new(@full_dictionary)
+      @cf = Array.new(@fd)
       @left = ('a'..'z').to_a
     end
 
-    def guess(word, guesses_left)
-      remove_from_dictionary_that_doesnt_match word
-      guesses_left > 4 ? select_vocals : select_character_by_frequence
+    def guess(word, gl)
+      dict word
+      gl > 3 ? by_v : by_f
       @left.pop
     end
     
-    def remove_from_dictionary_that_doesnt_match word
-      matcher = Regexp.new("^#{word.gsub("_", ".")}$")
-      @curr_dictionary.reject! { |w| !( matcher =~ w ) }
+    def dict word
+      m = Regexp.new("^#{word.gsub("_", ".")}$")
+      @cf.reject! { |w| !( m =~ w ) }
     end
     
-    def select_vocals
-      vocals = { "a" => 1, "e" => 0 }
-      vocals.default = 10
-      @left = @left.sort_by { |x| vocals[x] }
+    def by_v
+      @left = @left.sort_by { |x| @vocals[x] } 
     end
     
-    def select_character_by_frequence
+    def by_f
       @r = Hash.new(0)
-      character_frequency
-      sort_characters
+      char_f
+      sort_l
     end
 
-    def sort_characters
-      @left = @left.sort_by { |x| @r[x] || 0 }
+    def sort_l
+      @left = @left.sort_by { |x| @r[x] }
     end
 
-    def current_characters
-      @curr_dictionary.join.split(//)
+    def curr
+      @cf.join.split(//)
     end
     
-    def character_frequency
-      current_characters.each { |i| @r[i] = @r[i].next }
+    def char_f
+      curr.each { |i| @r[i] = @r[i].next }
     end
 
-    def word_list=(list) @full_dictionary = list end
-    def incorrect_guess(guess) @curr_dictionary.reject! { |w| w.include? guess } end
-    def correct_guess(guess) @curr_dictionary.reject! { |w| !w.include?(guess) } end
-    def fail(reason) ; end
-    def game_result(result, word); end
+    def word_list=(l) @fd = l end
+    def incorrect_guess(g) @cf.reject! { |w| w.include? g } end
+    def correct_guess(g) @cf.reject! { |w| !w.include?(g) } end
+    def fail(r) ; end
+    def game_result(r, w); end
 
   end
 end
