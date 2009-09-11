195b2b65c7dd421d883bef5b6f73a1cd0a9a0bb2
sunkencity <tux@well.com>
2009-09-11 10:18:09 +0200

    this version scores 99

--- lib/followthedata_hangman_player/followthedata_hangman_player.rb
+++ lib/followthedata_hangman_player/followthedata_hangman_player.rb
@@ -1,71 +1,49 @@
 module FollowthedataHangmanPlayer
   class FollowthedataHangmanPlayer
 
-    # You may initialize you player but the the initialize method must take NO paramters.
-    # The player will only be instantiated once, and will play many games.
-    def initialize
-    end
-
-    # Before starting a game, this method will be called to inform the player of all the possible words that may be
-    # played.
-    def word_list=(list)
-      @original_word_list = list
-    end
-
-    # a new game has started.  The number of guesses the player has left is passed in (default 6),
-    # in case you want to keep track of it.
     def new_game(guesses_left)
-      @word_list = Array.new(@original_word_list)
+      @curr_dictionary = Array.new(@full_dictionary)
       @left = ('a'..'z').to_a
     end
 
-    # Each turn your player must make a guess.  The word will be a bunch of underscores, one for each letter in the word.
-    # after your first turn, correct guesses will appear in the word parameter.  If the word was "shoes" and you guessed "s",
-    # the word parameter would be "s___s", if you then guess 'o', the next turn it would be "s_o_s", and so on.
-    # guesses_left is how many guesses you have left before your player is hung.
     def guess(word, guesses_left)
-      update_word_list_based_on(word)
-      guesses_left > 4 ? select_vocals : select_character(character_count())
+      remove_from_dictionary_that_doesnt_match word
+      guesses_left > 4 ? select_vocals : select_character_by_frequence
       @left.pop
     end
-
-    def select_character rating
-      @left = @left.sort_by { |x| rating[x] || 0 }
-    end
-
-    def select_vocals
-      @left = @left.sort_by { |x| %w{a e}.include?(x) ? 0 : 1 } #i o u y
-    end
-
-    def character_count
-      r = Hash.new(0)
-      @word_list.join.split(//).map { |i| r[i] = r[i].next }
-      r
-    end
-
-    def update_word_list_based_on word
+    
+    def remove_from_dictionary_that_doesnt_match word
       matcher = Regexp.new("^#{word.gsub("_", ".")}$")
-      @word_list.reject! { |w| !( matcher =~ w ) }
+      @curr_dictionary.reject! { |w| !( matcher =~ w ) }
+    end
+    
+    def select_vocals
+      @left = @left.sort_by { |x| %w{a e}.include?(x) ? 0 : 1 }
+    end
+    
+    def select_character_by_frequence
+      @r = Hash.new(0)
+      character_frequency
+      sort_characters
     end
 
-    # notifies you that your last guess was incorrect, and passes your guess back to the method
-    def incorrect_guess(guess)
-      @word_list.reject! { |w| w.include? guess }
+    def sort_characters
+      @left = @left.sort_by { |x| @r[x] || 0 }
     end
 
-    # notifies you that your last guess was correct, and passes your guess back to the method
-    def correct_guess(guess)
-      @word_list.reject! { |w| !w.include?(guess) }
+    def current_characters
+      @curr_dictionary.join.split(//)
+    end
+    
+    def character_frequency
+      current_characters.each { |i| @r[i] = @r[i].next }
     end
 
-    # you lost the game.  The reason is in the reason parameter
-    def fail(reason)
-    end
-
-    # The result of the game, it'll be one of 'win', 'loss', or 'fail'.
-    # The spelled out word will be provided regardless of the result.
-    def game_result(result, word)
-    end
+    def word_list=(list) @full_dictionary = list end
+    def incorrect_guess(guess) @curr_dictionary.reject! { |w| w.include? guess } end
+    def correct_guess(guess) @curr_dictionary.reject! { |w| !w.include?(guess) } end
+    def fail(reason) ; end
+    def game_result(result, word); end
 
   end
 end
