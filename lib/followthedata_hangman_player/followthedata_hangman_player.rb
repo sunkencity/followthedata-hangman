f26ff769d1e79b3723d5b98056ea4f759fe597f4
sunkencity <tux@well.com>
2009-09-14 13:26:20 +0200

    fixes

--- lib/followthedata_hangman_player/followthedata_hangman_player.rb
+++ lib/followthedata_hangman_player/followthedata_hangman_player.rb
@@ -1,47 +1,32 @@
 module FollowthedataHangmanPlayer
   class FollowthedataHangmanPlayer
 
-    def initialize
-      @vocals = { "a" => 1, "e" => 0 }
-      @vocals.default = 10
-    end
+    AZ = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
     
-    def new_game(guesses_left)
+    def new_game l
       @cf = Array.new(@fd)
-      @left = ('a'..'z').to_a
+      @lf = Array.new(AZ)
     end
 
-    def guess(word, gl)
-      dict word
-      gl > 3 ? by_v : by_f
-      @left.pop
+    def guess w, l
+      re w
+      by_f
+      @lf.pop.chr
     end
-    
-    def dict word
-      m = Regexp.new("^#{word.gsub("_", ".")}$")
+
+    def re c
+      m = Regexp.new("^#{c.gsub("_", ".")}$")
       @cf.reject! { |w| !( m =~ w ) }
     end
-    
-    def by_v
-      @left = @left.sort_by { |x| @vocals[x] } 
-    end
-    
+
     def by_f
       @r = Hash.new(0)
       char_f
-      sort_l
+      @lf = @lf.sort_by { |x| @r[x] }
     end
 
-    def sort_l
-      @left = @left.sort_by { |x| @r[x] }
-    end
-
-    def curr
-      @cf.join.split(//)
-    end
-    
     def char_f
-      curr.each { |i| @r[i] = @r[i].next }
+      @cf.join.each_byte { |i| @r[i] = @r[i].next }
     end
 
     def word_list=(l) @fd = l end
