19428088d5a84118aaae07572831de6d071e851b
sunkencity <tux@well.com>
2009-09-16 18:45:28 +0200

    fixes

--- lib/followthedata_hangman_player/followthedata_hangman_player.rb
+++ lib/followthedata_hangman_player/followthedata_hangman_player.rb
@@ -1,37 +1,37 @@
 module FollowthedataHangmanPlayer
   class FollowthedataHangmanPlayer
 
-    AZ = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
-    
+    A = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
     def new_game l
-      @cf = Array.new(@fd)
-      @lf = Array.new(AZ)
+      @w = Array.new(@f)
+      @l = Array.new(A)
+      @r = Array.new(123, 0)
     end
 
     def guess w, l
-      re w
-      by_f
-      @lf.pop.chr
+      a w
+      b
+      @l.pop.chr
     end
 
-    def re c
-      m = Regexp.new("^#{c.gsub("_", ".")}$")
-      @cf.reject! { |w| !( m =~ w ) }
+    def a c
+      m = Regexp.new('^' << c.gsub('_','.') << '$')
+      @w.reject! { |w| m !~ w }
     end
 
-    def by_f
-      @r = Hash.new(0)
-      char_f
-      @lf = @lf.sort_by { |x| @r[x] }
+    def b
+      @r.fill(0)
+      c
+      @l = @l.sort_by { |x| @r[x] }
     end
 
-    def char_f
-      @cf.join.each_byte { |i| @r[i] = @r[i].next }
+    def c
+      @w.join.each_byte { |i| @r[i] = @r[i].next }
     end
 
-    def word_list=(l) @fd = l end
-    def incorrect_guess(g) @cf.reject! { |w| w.include? g } end
-    def correct_guess(g) @cf.reject! { |w| !w.include?(g) } end
+    def word_list=(l) @f = l end
+    def incorrect_guess(g) @w.reject! { |w| w.include? g } end
+    def correct_guess(g) @w.reject! { |w| !w.include?(g) } end
     def fail(r) ; end
     def game_result(r, w); end
 
